2fac905f1ed8ff6ed55b860295422d3e536ffc62
guille.rodriguez <guilleentropia>
2015-09-09 16:28:49 -0300

    construyendo la venta

--- ProyectoFinal.MVC5/Controllers/VentaController.cs
+++ ProyectoFinal.MVC5/Controllers/VentaController.cs
@@ -11,19 +11,30 @@ namespace ProyectoFinal.MVC5.Controllers
     public class VentaController : Controller
     {
         private readonly ITerceroAppService _terceroAppService;
+        private readonly IFormaPagoAppService _formapagoAppService;
 
-        public VentaController(ITerceroAppService terceroAppService)
+        public VentaController(ITerceroAppService terceroAppService, IFormaPagoAppService formapagoAppService)
         {
             _terceroAppService = terceroAppService;
+            _formapagoAppService = formapagoAppService;
         }
 
         // GET: Venta
         public ActionResult Index()
         {
             ViewBag.Clientes = new SelectList(_terceroAppService.ObtenerTodo(), "Id", "NombreCompleto");
+            ViewBag.FormaPago = new SelectList(_formapagoAppService.ObtenerTodo(), "Id", "Descripcion");
             ViewBag.Productos = (List<Item>)Session["cart"];
             ViewBag.Fecha = DateTime.Now;
             var a = Session["User"];
+            return View();
+        }
+
+        
+        public ActionResult Venta(string fecha, string usuario, double total)
+        {
+
+
             return View();
         }
 
@@ -33,5 +44,15 @@ namespace ProyectoFinal.MVC5.Controllers
 
             return Json(_terceroAppService.ObtenerTodo(), JsonRequestBehavior.AllowGet);
         }
+
+
+        public JsonResult ObtenerFormaPago()
+        {
+
+            return Json(_formapagoAppService.ObtenerTodo(), JsonRequestBehavior.AllowGet);
+        }
+
+
+
     }
 }
